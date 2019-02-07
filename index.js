7b2174193477933098502594468db9aa1b455217
nemanjan00 <nemanjan00@gmail.com>
2019-02-07 20:14:20 +0100

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+const request = require("request");
+
+module.exports = {
+	getUser: (username) => {
+		return new Promise((resolve, reject) => {
+			request.get("https://www.instagram.com/" + username + "/", (err, _, body) => {
+				if(err){
+					reject(err);
+				}
+
+				let json = body.split("window._sharedData = ");
+
+				if(json.length > 1){
+					json = json[1];
+
+					json = json.split(";");
+
+					if(json.length > 1){
+						json = json[0];
+
+						resolve(JSON.parse(json));
+					} else {
+						reject("Something changed on instagram? ");
+					}
+				} else {
+					reject("Something changed on instagram? ");
+				}
+			});
+		});
+	}
+}
+
